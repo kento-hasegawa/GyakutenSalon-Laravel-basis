5d8b3ef682ce4044dc82d50bad03985dc9fcf308
EC2 Default User <ec2-user@ip-172-31-11-92.us-east-2.compute.internal>
2020-12-20 13:13:00 +0000

    laravel basis3

---
+++ tests/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang = "en" >
+<head>
+    <meta charaset="utf-8">
+    <meta name="view-point" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>人生逆転サロン</title>
+</head>
+<body>
+    <header class= "navbar nabibar-dark bg-danger">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('')}}">
+                Gyakuten
+            </a>
+        </div>
+    </header >
+    <div class="container mt-4">
+       <div class="card mt-4">
+            <div class="card-header mb-2">
+            title
+            </div>
+            <div class="card-body">
+                <p class="card-text">
+                    
+                </p>
+            
+            </div>
+            <div class="card-footer">
+                <span class="mr-2"
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
