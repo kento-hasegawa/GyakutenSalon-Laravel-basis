6e4b80bea5707ddb97054c5251f225bc36c77a13
EC2 Default User <ec2-user@ip-172-31-11-92.us-east-2.compute.internal>
2020-12-23 11:49:52 +0000

    Laravel basis4

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,15 +3,16 @@
 @section('content')
 
  <div class="container mt-4">
+       @foreach ($posts as $post)
        <div class="card mt-4">
             <div class="card-header mb-2">
-            title
+                {{ $post->title }}
             </div>
             <div class="card-body">
                 <p class="card-text">
-                    body
+                 {{ $post->body }}
                 </p>
-            
+                
             </div>
             <div class="card-footer">
                 <span class="mr-2">
@@ -19,6 +20,8 @@
                 </span>
             </div>
         </div>
+        @endforeach    
     </div>
+
 
 @endsection('content')
